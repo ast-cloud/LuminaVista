658f14dafb357628527cb3b0cffeadfdac2d514f
Aastik <aastikyadav1997.ay@gmail.com>
2024-08-18 08:18:35 +0530

    Added index.js

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+const db = require('./db');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const contactRoutes = require('./routes/contactRoute');
+const mentorRoutes = require('./routes/mentorRoute');
+const internRoutes = require('./routes/internRoute');
+const userDetailsRoutes = require('./routes/userDetailsRoute');
+const eventDetailsRoutes = require('./routes/eventDetailsRoute');
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.send('Welcome to our lumina vista app!');
+})
+
+app.use('/', contactRoutes);
+app.use('/', mentorRoutes);
+app.use('/', internRoutes);
+app.use('/', userDetailsRoutes);
+app.use('/', eventDetailsRoutes);
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+})
