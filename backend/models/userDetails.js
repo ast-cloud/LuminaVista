5bfcacede7be77266ed84f4a715ff83d94026525
Aastik <aastikyadav1997.ay@gmail.com>
2024-08-18 08:40:11 +0530

    Changed regsitrationDate field type in mongodb model

--- backend/models/userDetails.js
+++ backend/models/userDetails.js
@@ -22,7 +22,7 @@ const userDetailsSchema = new mongoose.Schema({
         required: true,
     },
     registrationDate: {
-        type: Date,
+        type: String,
         required: true,
     },
     comments: {
